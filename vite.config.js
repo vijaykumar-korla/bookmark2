26c1fd882e9e84d5c7e612e83790771dcd8afcf5
VIJAY KUMAR KORLA <k.vijaykumar1749@gmail.com>
2026-07-13 21:55:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Dynamically switches the base path depending on the deployment platform
+  base: process.env.VERCEL ? '/' : '/bookmark2/',
   plugins: [react()],
 })

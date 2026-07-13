e070743874e212831ae844b301a80fca17e071c5
VIJAY KUMAR KORLA <k.vijaykumar1749@gmail.com>
2026-07-13 22:19:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Use root '/' for Vercel, and use your repository subfolder path for GitHub Pages
+  base: process.env.GITHUB_PAGES ? '/bookmark2/' : '/',
   plugins: [react()],
 })
